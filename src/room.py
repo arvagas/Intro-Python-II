6ed9f5b6835bca76e55f9e8dd3b2ce2160bacfff
Arvin Agas <arvagas@gmail.com>
2019-12-03 18:39:29 -0500

    removed unneeded attribures in room class

--- src/room.py
+++ src/room.py
@@ -5,13 +5,4 @@ class Room:
         self.name = name
         self.description = description
     # def __str__(self):
-    #     return f'{self.name}: {self.description}'
-
-    def n_to(self, north_room):
-        self.n_to = north_room
-    def s_to(self, south_room):
-        self.s_to = south_room
-    def e_to(self, east_room):
-        self.e_to = east_room
-    def w_to(self, west_room):
-        self.w_to = west_room
+    #     return f'{self.name}'
